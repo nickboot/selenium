b157cc47f88bf6bf6c4a82c44b9e862b3ef58243
nickboot <nickbooty50@gmail.com>
2018-08-12 22:36:57 +0800

    browser.set_window_size(1920, 1080) 设置浏览器窗口大小

--- lpl.py
+++ lpl.py
@@ -6,8 +6,9 @@ from time import sleep
 browser = webdriver.Chrome()    #调用谷歌浏览器
 url = "http://lpl.qq.com"
 browser.get(url)
-browser.maximize_window()       #浏览器窗口最大化
+#browser.maximize_window()       #浏览器窗口最大化
+browser.set_window_size(1920, 1080)
 browser.implicitly_wait(30)     #隐式等待30s
-sleep(2)
+sleep(5)
 print ("退出浏览器")
 browser.quit()
