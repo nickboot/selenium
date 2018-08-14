fbedd03ce3cff8170454104b8fc6c0dd5316e969
nickboot <nickbooty50@gmail.com>
2018-08-14 12:40:24 +0800

    使用QQ登录 iframe 内嵌框架切换

--- lpl.py
+++ lpl.py
@@ -12,7 +12,18 @@ url = "http://lpl.qq.com"
 browser.get(url)
 #browser.maximize_window()       #浏览器窗口最大化
 browser.set_window_size(1920, 1080)
-browser.implicitly_wait(30)     #隐式等待30s
-sleep(5)
+browser.implicitly_wait(10)     #隐式等待30s
+sleep(1)
+browser.find_element_by_xpath('//*[@id="unlogin"]/a').click()      #登录
+"""
+弹出框 iframe 切换
+browser.switch_to.frame(browser.find_element_by_id("loginIframe")) 
+browser.switch_to.default_content()
+"""
+browser.switch_to.frame(browser.find_element_by_id("loginIframe"))      #切换到QQ登录页面
+#browser.find_element_by_id("nick_2256395943").click()           #点击QQ昵称
+browser.find_element_by_id("img_out_2256395943").click()        #点击QQ头像
+
+sleep(10)
 print ("退出浏览器")
 browser.quit()
